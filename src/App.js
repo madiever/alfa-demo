8c36fdd36ada6b79bcb4c1327dc7fa1cb4613d0b
Erzhan <madieverr@gmail.com>
2021-04-22 16:56:50 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
     return (
         <div className="App">
             <Sidebar/>
-            <div className='body'>
+            <main>
                 <div className="container">
                     <ProfileHeader/>
                     <hr className='line' />
                     <EmployeeData />
                     <ProjectData />
                 </div>
-            </div>
+            </main>
         </div>
     );
 }
